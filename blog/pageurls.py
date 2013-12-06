c43724db870bcf3f51d0e10d05421f24d50cf3d4
Aleksandr <greenteamer@bk.ru>
2013-12-07 03:20:21 +0700

    конец экспериментам с редиректом

--- blog/pageurls.py
+++ blog/pageurls.py
@@ -1,10 +1,14 @@
 #coding: utf-8
 from django.conf.urls import patterns, url
 from blog.views import PageListView, PageDetailView, ReviewListView
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     url(r'^$', PageListView.as_view(), name='list'),
-    # url(r'^(?P<pk>\d+)/$', PageDetailView.as_view(), name='detail'),
+
+    # redirect
+    # url(r'^(?P<pk>\d+)/$', PageRedirectView.as_view(), name='detail'),
+
     url(r'^review/',ReviewListView.as_view(), name='review'),
     url(r'^(?P<slug>[-_\w]+)/$', PageDetailView.as_view(), name='detail'),
 )
