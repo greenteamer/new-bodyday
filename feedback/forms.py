bc7c46cec80a04fb8a287b2479081c0f167039d7
Aleksandr <greenteamer@bk.ru>
2014-01-05 22:48:31 +0700

    добавленна возможность отправки единичных html писем

--- feedback/forms.py
+++ feedback/forms.py
@@ -7,8 +7,8 @@ class ContactForm(forms.Form):
     subject = forms.CharField(label=u'Ваше имя', max_length=250)
     phone = forms.CharField(label=u'Ваш телефон', max_length=250)
     sender = forms.EmailField(label=u'Ваша почта', required=False)
-    # message = forms.CharField(label=u'Сообщение', max_length=500,widget=forms.Textarea(attrs={'rows': 5, 'cols': 50}))
-    html_content = forms.CharField(label=u'Сообщение', max_length=500,widget=forms.TextInput())
+    message = forms.CharField(label=u'Сообщение', max_length=500,widget=forms.Textarea(attrs={'rows': 5, 'cols': 50}))
+    # html_content = forms.CharField(label=u'Сообщение', max_length=500,widget=forms.TextInput())
     #response = forms.CharField(label=u'Response text', max_length=500,widget=forms.Textarea(attrs={'rows': 5, 'cols': 50}))
     # cc_myself = forms.BooleanField(required=False)
 
